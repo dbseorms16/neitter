ef32357634f779ae6a2214bdf64c16423eea468b
dbseorms16 <49383494+dbseorms16@users.noreply.github.com>
2020-10-22 09:31:19 +0900

    Update fBase.js

--- src/fBase.js
+++ src/fBase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqa3zJKsc8ah9RBbTwOb8vhQzBIm12-iQ",
-  authDomain: "nwitter-db806.firebaseapp.com",
-  databaseURL: "https://nwitter-db806.firebaseio.com",
-  projectId: "nwitter-db806",
-  storageBucket: "nwitter-db806.appspot.com",
-  messagingSenderId: "25531543845",
-  appId: "1:25531543845:web:95b76efa34c8f5eece6acc",
+  apiKey: //config,
+  authDomain: //config,
+  databaseURL: //config,
+  projectId: //config,
+  storageBucket: //config,
+  messagingSenderId: //config,
+  appId: //config,
 };
 
 firebase.initializeApp(firebaseConfig);
